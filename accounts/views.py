1da0e1dc149d5d3023307a585305978022777321
mrmohsensami <mrmohsensami@gmail.com>
2022-07-02 12:39:46 +0430

    Show Register Form and add OTP model

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
+from urllib import request
 from django.shortcuts import render
+from django.views import View
+from .forms import UserRegistrationForm
 
-# Create your views here.
+
+class RegisterView(View):
+    form_class = UserRegistrationForm
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, 'accounts/register.html', {'form': form})
+
+    def post(self):
+        pass
